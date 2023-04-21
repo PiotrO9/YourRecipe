7b683dcf5b46618306c254b220f70cef413f771e
PiotrO9 <piotr.ostrowski02@wp.pl>
2023-04-21 18:38:41 +0200

    Move small components to header component and made span with name and icon

---
+++ src/components/Common-components/NameContainer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="NameCotainer">
+    <span>
+        Piotr
+    </span>
+    <span>
+        Ostrowski
+    </span>
+  </div>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style lang="scss">
+
+</style>
--- src/components/Core-components/AppHeader.vue
+++ src/components/Core-components/AppHeader.vue
@@ -1,24 +1,75 @@
 <template>
   <header>
-    <LogoContainer />
+    <div class="Header__logo">
+        <span>
+            Twoje
+        </span>
+        <span>
+            Przepisy
+        </span>
+    </div>
+    <div class="Header__name">
+        <img src="../../../public/cook-book.png" alt="Recipe book">
+        <span>
+            Piotr Ostrowski
+        </span>
+    </div>
   </header>
 </template>
 
 <script lang="ts">
-import LogoContainer from '../Common-components/LogoContainer.vue'
 import { defineComponent } from 'vue';
 
-
 export default defineComponent ({
-    components: {
-        LogoContainer
-    }
+    
 })
 </script>
 
 <style lang="scss" scoped>
+    @import '@/GlobalStyles/variabless.scss';
+    
     header {
         width: 100%;
-        height: 10%;
+        height: 6%;
+        min-width: 400px;
+        display: flex;
+        justify-content: space-between;
+        font-family: 'Sora';
     }
+
+    .Header__logo {
+        height: 100%;
+        display: flex;
+        align-items: center;
+        margin-left: 15px;
+        gap: 10px;
+
+        span {
+            font-weight: bold;
+            
+            &:first-child {
+                color: red;
+                font-size: 22px;
+            }
+        }
+    }
+
+    .Header__name {
+        display: flex;
+        align-items: center;
+        margin-right: 15px;
+        gap: 10px;
+        
+        img {
+            @include SquareSize(35px);
+            margin-top: 10px;
+        }
+
+        span {
+            font-size: 20px;
+            margin-top: 20px;
+            word-spacing: 7px;
+        }
+    }
+
 </style>
