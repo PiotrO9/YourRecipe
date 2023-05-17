d832f2f7c56a3202ee25956bdbac7b39f1b9197a
PiotrO9 <piotr.ostrowski02@wp.pl>
2023-05-17 18:24:38 +0200

    Add content in form of adding recipe view

--- src/views/AddingRecipeView.vue
+++ src/views/AddingRecipeView.vue
@@ -3,10 +3,13 @@
     <h1>Dodaj nowy przepis</h1>
     <hr>
     <div class="mainContent__title">
-      <p>Tytuł</p>
-      <input type="text" placeholder="Nazwa przepisu">
+      <p>Tytuł *</p>
+      <input type="text" 
+        placeholder="Nazwa przepisu" 
+        autocomplete="none">
     </div>
     <div class="mainContent__difficulty">
+      <p>Poziom trudności *</p>
       <div class="mainContent__difficulty--stars">
         <SetStar :StarHover="HandleStarHover" 
                 :StarClick="HandleStarClick"/>
@@ -21,13 +24,15 @@
       </div>
     </div>
     <div class="mainContent__time">
-
+      <p>Czas trwania *</p>
+      <input type="number" min="5" placeholder="5 min">
     </div>
     <div class="mainContent__description">
-
+      <p>Opis</p>
+      <textarea placeholder="Opis przepisu..."></textarea>
     </div>
     <div class="mainContent__ingredients">
-      
+      <p>Składniki *</p>
     </div>
     <button>
       Dodaj
@@ -70,24 +75,57 @@ main {
   h1 {
     text-align: center;
     margin-top: 1rem;
-    margin-bottom: 1rem
+    margin-bottom: 1rem;
   }
 
   hr {
     width: 90%;
     margin-top: 1rem;
-    margin-bottom: 1rem
+    margin-bottom: 1rem;
   }
 
   .mainContent {
 
+    &__title {
+      width: 25%;
+      @include flex-center;
+      flex-direction: column;
+      margin-bottom: 2rem;
+
+      p {
+        font-size: 1.5rem;
+        margin: 3px;
+        align-self: flex-start;
+      }
+
+      input {
+        width: 100%;
+        min-height: 40px;
+        padding-left: 10px;
+        color: black;
+        font-size: 100%;
+        outline: none;
+        border: 2px solid $Orange;
+        border-radius: 8px;
+      }
+    }
+
     &__difficulty {
-      height: 20%;
+      @include flex-center;
+      width: 25%;
+      height: 10%;
+      flex-direction: column;
+      margin-bottom: 2rem;
+
+      p {
+          font-size: 1.6rem
+      }
 
       &--stars {
         display: flex;
         flex-direction: row;
         gap: 20px;
+
         .SetStar {
           @include SquareSize(35px);
           display: flex;
@@ -104,8 +142,74 @@ main {
         }
       }
     }
+
+    &__time {
+      width: 25%;
+      @include flex-center;
+      flex-direction: column;
+      margin-bottom: 2rem;
+
+      p {
+        font-size: 1.5rem;
+        align-self: flex-start;
+        margin: 4px;
+      }
+
+      input {
+        width: 100%;
+        height: 40px;
+        outline: none;
+        border: 2px solid $Orange;
+        border-radius: 8px;
+        font-size: 100%;
+        padding-left: 0.7rem;
+      }
+    }
+
+    &__description {
+      @include flex-center;
+      width: 25%;
+      height: 15%;
+      flex-direction: column;
+      margin-bottom: 2rem;
+
+      p {
+        align-self: flex-start;
+        font-size: 1.5rem;
+        margin: 4px;
+      }
+
+      textarea {
+        width: 100%;
+        height: 80%;
+        outline: none;
+        border: 2px solid $Orange;
+        border-radius: 8px;
+        padding: 4px;
+      }
+    }
+
+    &__ingredients {
+      width: 25%;
+      @include flex-center;
+      flex-direction: column;
+      margin-bottom: 2rem;
+
+      p {
+        font-size: 1.5rem;
+      }
+    } 
   }
 
+  button {
+    width: 8%;
+    height: 40px;
+    min-width: 100px;
+    outline: none;
+    border: 2px solid $Orange;
+    border-radius: 8px;
+    color: black;
+    font-size: 140%;
+  }
 }
-
 </style>
